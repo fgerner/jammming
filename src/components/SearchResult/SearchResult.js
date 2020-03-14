9e2d7d0cc326b8ae23817addfb365cf9bad737ba
Fred <fred.gerner@icloud.com>
2020-03-14 13:23:16 +1100

    added search result

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {TrackList} from '../TrackList/TrackList';
+import './SearchResult.css';
+
+class SearchResult extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList />
+      </div>
+
+    )
+  }
+}
+
+export default SearchResult;
